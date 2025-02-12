7054c316e2c3c593c5f94b39b78244c500e06325
ethanevans33 <etevans2327@gmail.com>
2025-02-12 00:38:53 -0600

    Added comments to the already existing code

--- src/g18/LibraryApp.java
+++ src/g18/LibraryApp.java
@@ -6,18 +6,22 @@ import g18.Class.Library;
 public class Main {
 
 	public static void main(String[] args) {
+		// Creates a new Library object
 		Library library = new Library();
-		
+
+		// Creates three new Book objects
 		Book book1 = new Book("Clean Code", "Robert C. Martin", "978-0135166307", 40.99);
 		Book book2 = new Book("Design Patterns", "Erich Gamma", "978-0201633610", 50.99);
 		Book book3 = new Book("The Pragmatic Programmer", "Andrew Hunt", "978-0135957059", 45.99);
-		
+
+		// Adds the three books to the book array in library
 		System.out.println("Adding books...");
 		library.addBook(book1);
 		library.addBook(book2);
 		library.addBook(book3);
 		System.out.println("Book added successfully.");
-		
+
+		// Creates a ISBN to search for and then searches the book array for a matching ISBN
 		String searchISBN = "978-0135166307";
 		System.out.println("\nSearching for book with ISBN: " + searchISBN);
 		Book foundBook = library.searchByISBN(searchISBN);
@@ -27,16 +31,20 @@ public class Main {
 		else {
 			System.out.println("Book not found");
 		}
-		
+
+		// Lists the books in the book array
 		System.out.println();
 		library.displayBooks();
-		
+
+		// Removes book2 from the book array
 		System.out.println("\nRemoving book: " + book2.toString());
 		library.removeBook(book2);
-		
+
+		// Lists the books in the book array
 		System.out.println();
 		library.displayBooks();
-		
+
+		// Tries to remove a book from the book array that does not exist
 		System.out.println();
 		if(!library.removeBook(book2)) {
 			System.out.println("Cannot remove book \"" + book2.toString() + "\", book does not exist");
