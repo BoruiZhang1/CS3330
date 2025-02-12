9c7501f013f3f90a6c9c4cb60e5f84ff9d69937c
Joe520088 <joe520088@gmail.com>
2025-02-12 14:12:11 -0600

    Fixed File of Book Class but there's no comment

--- src/g18/Class/Book.java
+++ src/g18/Class/Book.java
@@ -6,6 +6,50 @@ public class Book {
 	private String ISBN;
 	private double price;
 	
+	// default constructor
+	public Book()
+	{
+		this.title ="Unknown";
+		this.author = "Unknown";
+		this.ISBN = "Unknown";
+		this.price = 0.0;
+	}
+	
+	// construct to take in value 
+		public Book(String title, String author, String ISBN, Double price)
+		{
+			this.title = title;
+			this.author = author;
+			this.ISBN = ISBN;
+			this.price = price;
+		}
+		
+		// copy constructor
+		public Book(Book other)
+		{
+			this.title = other.title;
+			this.author = other.author;
+			this.ISBN = other.ISBN;
+			this.price = other.price;
+		}
+		
+		@Override
+		public boolean equals(Object other)
+		{
+			Book book = (Book) other;
+			if(this == other)
+			{
+				return true;
+			}
+			
+			return this.ISBN.equals(book.ISBN);
+		}
+		
+		@Override
+		public String toString() {
+			return title + " by " + author + " (ISBN: " + ISBN + ", $" + price + ")";
+		}
+	
 	
 	public String getTitle() {
 		return title;
